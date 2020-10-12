ec6e6b2c07af003c96384e58c713854654a938f7
d8040 <d8040@nate.com>
2020-10-12 16:50:25 +0900

    고객에게 명령어 받기

--- src/com/sbs/example/laptopexam/Main.java
+++ src/com/sbs/example/laptopexam/Main.java
@@ -1,8 +1,26 @@
 package com.sbs.example.laptopexam;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("명령어) ");
+		
+		String command = scanner.nextLine();
+		
+		System.out.println(command);
+	
+		if (command.equals("article add")) {
+			System.out.println("게시물 추가");
+		} else if (command.equals("article list")) {
+			System.out.println("게시물 목록");
+		} else {
+			System.out.println("잘못된 명령어");
+		}
+
+		scanner.close();
 	}
 
 }
