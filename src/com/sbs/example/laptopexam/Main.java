648dce91b7391d4b9f4d34091d4b8ccbec33d6a3
d8040 <d8040@nate.com>
2020-10-12 17:48:35 +0900

    while문 활용 반복실행

--- src/com/sbs/example/laptopexam/Main.java
+++ src/com/sbs/example/laptopexam/Main.java
@@ -6,20 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.printf("명령어) ");
-		
-		String command = scanner.nextLine();
-		
-		System.out.println(command);
-	
-		if (command.equals("article add")) {
-			System.out.println("게시물 추가");
-		} else if (command.equals("article list")) {
-			System.out.println("게시물 목록");
-		} else {
-			System.out.println("잘못된 명령어");
-		}
 
+		while (true) {
+			System.out.printf("명령어) ");
+			String command = scanner.nextLine();
+			if (command.equals("article add")) {
+				System.out.println("게시물 추가");
+			} else if (command.equals("article list")) {
+				System.out.println("게시물 목록");
+			} else if (command.equals("exit")) {
+				System.out.println("프로그램 종료");
+				break;
+			} else {
+				System.out.println("잘못된 명령어");
+			}
+
+		}
 		scanner.close();
 	}
 
